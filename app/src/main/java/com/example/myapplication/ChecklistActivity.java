540c118fb217cc40be2092ed9605fefb7da7e053
Yu Liu <53040839+Leosssss@users.noreply.github.com>
2022-01-17 23:23:44 +0100

    Update ChecklistActivity(Die Besprechung beginnt nur, wenn der Checklist-Item angekreuzt ist)

--- app/src/main/java/com/example/myapplication/ChecklistActivity.java
+++ app/src/main/java/com/example/myapplication/ChecklistActivity.java
@@ -2,21 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class ChecklistActivity extends AppCompatActivity {
+
+    //checklist Checkbox
+    private CheckBox cbTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
+        cbTest = (CheckBox) findViewById(R.id.checkBox);
     }
 
     // open the new activity
     public void openCountdownActivity(View view) {
         Intent intent = new Intent(this, CountdownActivity.class);
-        startActivity(intent);
+        if(cbTest.isChecked()){//Meeting will only start if checklist item is selected
+            startActivity(intent);
+        }
     }
 }
