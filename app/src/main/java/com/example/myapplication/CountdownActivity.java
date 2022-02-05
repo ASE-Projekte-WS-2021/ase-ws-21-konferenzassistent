7bca3a99e4630d655273b6942b7abaa7c7495215
Johannes Hoffmann <46888078+jmhoffmann9612@users.noreply.github.com>
2022-02-05 12:44:41 +0100

    Einfache Startseite implementiert
    MainActivity zu SettingsActivity umbenannt
    Neue MainActivity; gesetzt als Startseite
    Neue MainActivity Funktionalität
    Zurück-Button für SettingsActivity
    Meeting beenden führt zur Main-, statt SettingsActivity

--- app/src/main/java/com/example/myapplication/CountdownActivity.java
+++ app/src/main/java/com/example/myapplication/CountdownActivity.java
@@ -1,14 +1,12 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.IntentCompat;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
