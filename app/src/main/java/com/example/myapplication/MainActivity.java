b2ba456bf7f710b27cd2cd6c9e047330e232c603
Yu Liu <53040839+Leosssss@users.noreply.github.com>
2022-03-11 00:49:49 +0100

    activity main view

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
+
+    public void openPastMeeting(View view){
+        Intent intent = new Intent(this, PastMeetingInfoActivity.class);
+        startActivity(intent);
+    }
 }
