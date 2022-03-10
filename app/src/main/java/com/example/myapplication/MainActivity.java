1667802c3dc2fb86a61a64e31a9cd138717c5c78
Johannes Hoffmann <46888078+jmhoffmann9612@users.noreply.github.com>
2022-03-10 17:15:03 +0100

    Functionality and Buttons for testing
    Remove buttons before merge into dev

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,17 +1,79 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvMeetings;
+    private TextView introText;
+
+    private ArrayList<String> meetingsList;
+    MeetingHistoryAdapter meetingHistoryAdapter;
+    LinearLayoutManager linearLayoutManager;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvMeetings = findViewById(R.id.rv_history);
+        introText = findViewById(R.id.introText);
+
+        meetingsList = new ArrayList<>();
+        meetingsList.add("test");
+
+        meetingHistoryAdapter = new MeetingHistoryAdapter(this,meetingsList);
+        rvMeetings.setAdapter(meetingHistoryAdapter);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        rvMeetings.setLayoutManager(linearLayoutManager);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setIntroTextVisibility();
+    }
+
+    private void onDataChanged() {
+        meetingHistoryAdapter.notifyDataSetChanged();
+        setIntroTextVisibility();
+        linearLayoutManager.scrollToPosition(meetingsList.size() - 1);
+    }
+
+    private void setIntroTextVisibility() {
+        if (meetingsList.isEmpty()) {
+            introText.setVisibility(View.VISIBLE);
+        } else {
+            introText.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void onButtonTestPlusClicked(View view) {
+        meetingsList.add("test" + counter);
+        counter++;
+        onDataChanged();
+    }
+
+    public void onButtonTestMinusClicked(View view) {
+        if (meetingsList.isEmpty()) {
+            Toast.makeText(this,"List empty",Toast.LENGTH_SHORT).show();
+        } else {
+            meetingsList.remove(meetingsList.size() - 1);
+            onDataChanged();
+        }
     }
 
     public void openSettingsActivity(View view) {
---
+++ app/src/main/java/com/example/myapplication/MeetingHistoryAdapter.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MeetingHistoryAdapter extends RecyclerView.Adapter<MeetingHistoryAdapter.MeetingHistoryViewHolder> {
+
+    private Context ct;
+    private ArrayList<String> meetingsList;
+
+    public MeetingHistoryAdapter(Context ct, ArrayList<String> meetingsList) {
+        this.ct = ct;
+        this.meetingsList = meetingsList;
+    }
+
+    @NonNull
+    @Override
+    public MeetingHistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(ct);
+        View view = inflater.inflate(R.layout.meeting_history_row,parent,false);
+        return new MeetingHistoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MeetingHistoryViewHolder holder, int position) {
+        holder.tvTitle.setText(meetingsList.get(position));
+
+        holder.cardView.setOnClickListener(view -> {
+            Intent intent = new Intent(ct, PastMeetingInfoActivity.class);
+            ct.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return meetingsList.size();
+    }
+
+    public class MeetingHistoryViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cardView;
+        TextView tvTitle;
+
+        public MeetingHistoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitleRow);
+            cardView = itemView.findViewById(R.id.cvRow);
+        }
+
+    }
+}
