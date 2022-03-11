7566225c9bd440c3ffc8a729bbb3d43c996a5692
Tobias Zels <T.smi@gmx.de>
2022-03-11 16:33:47 +0100

    Implementiert den Aufruf der Informationsseite

--- app/src/main/java/com/example/myapplication/CountdownActivity.java
+++ app/src/main/java/com/example/myapplication/CountdownActivity.java
@@ -14,6 +14,14 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class CountdownActivity extends AppCompatActivity {
 
     // Countdown TextView
@@ -47,26 +55,24 @@ public class CountdownActivity extends AppCompatActivity {
     private ProgressBar abstandsProgressBar;
     private ProgressBar lueftungsProgressBar;
 
+    private TextView teilnehmerTextView;
+    private TextView ortTextView;
+
+    private Date startDate;
+    private Date endDate;
+
+    private int participants = 0;
+    private String ort;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown);
-
         getSupportActionBar().setTitle("Meeting");
 
-        countdownText = findViewById(R.id.countdownView);
-        startCountdownButton = findViewById((R.id.StartButton));
+        initiateComponents();
 
-        abstandsPauseButton = findViewById(R.id.abstandsPauseButton);
-        countdownPauseButton = findViewById(R.id.PauseButton);
-
-        abstandsButton = findViewById(R.id.abstandsButton);
-        abstandsView = findViewById(R.id.abstandsView);
-
-        lueftungsInfoText = findViewById(R.id.lueftungsInfoText);
-
-        abstandsProgressBar = findViewById(R.id.abstandsProgressBar);
-        lueftungsProgressBar = findViewById(R.id.lueftungsProgressBar);
+        startDate = new Date();
 
         // get max Countdown from intent
         maxCountdownTime = getIntent().getLongExtra("maxCountdownTime", 0) * 60000;
@@ -76,7 +82,14 @@ public class CountdownActivity extends AppCompatActivity {
 
         lueftungsSwitchStatus = getIntent().getBooleanExtra("lueftungsSwitchStatus", false);
         abstandsSwitchStatus = getIntent().getBooleanExtra("abstandsSwitchStatus", false);
+        participants = getIntent().getIntExtra("participants", 0);
+        ort = getIntent().getStringExtra("ort");
 
+        /* DEBUG */
+        participants = 9;
+        ort = "Regensburg 1";
+
+        FillInformationField("" + participants, ort);
 
         // start the Countdown service
         Intent countdownIntent = new Intent(this, CountdownService.class);
@@ -99,6 +112,31 @@ public class CountdownActivity extends AppCompatActivity {
         hideUI();
     }
 
+    private void FillInformationField(String teilnehmer, String ort){
+        teilnehmerTextView.setText(teilnehmer);
+        ortTextView.setText(ort);
+    }
+
+    // Finds the Components in the View
+    private void initiateComponents(){
+        countdownText = findViewById(R.id.countdownView);
+        startCountdownButton = findViewById((R.id.StartButton));
+
+        abstandsPauseButton = findViewById(R.id.abstandsPauseButton);
+        countdownPauseButton = findViewById(R.id.PauseButton);
+
+        abstandsButton = findViewById(R.id.abstandsButton);
+        abstandsView = findViewById(R.id.abstandsView);
+
+        lueftungsInfoText = findViewById(R.id.lueftungsInfoText);
+
+        abstandsProgressBar = findViewById(R.id.abstandsProgressBar);
+        lueftungsProgressBar = findViewById(R.id.lueftungsProgressBar);
+
+        teilnehmerTextView = findViewById(R.id.teilnehmerTextView);
+        ortTextView = findViewById(R.id.ortTextView);
+    }
+
 
     // Sets the Ui to Invisible if not used
     private void hideUI(){
@@ -108,12 +146,14 @@ public class CountdownActivity extends AppCompatActivity {
             startCountdownButton.setVisibility(View.GONE);
             lueftungsProgressBar.setVisibility(View.GONE);
             lueftungsInfoText.setVisibility(View.GONE);
+            findViewById(R.id.materialCardView3).setVisibility(View.GONE);
         }
         // If "Abstand" is disabled
         if(!abstandsSwitchStatus){
             abstandsView.setVisibility(View.GONE);
             abstandsButton.setVisibility(View.GONE);
             abstandsProgressBar.setVisibility(View.GONE);
+            findViewById(R.id.materialCardView4).setVisibility(View.GONE);
         }
 
     }
@@ -307,11 +347,30 @@ public class CountdownActivity extends AppCompatActivity {
     * "To clear top activities from stack use below code
     * It will delete all activities from stack either asynctask run or not in the application." */
 
+    private void SaveToDatabase(){
+        endDate = new Date();
+        long diff = endDate.getTime() - startDate.getTime();
+        long seconds = diff / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Berlin"));
+
+        MettingDatabase database = new MettingDatabase(this);
+        database.addMeeting("" + dateFormat.format(startDate) , "" + seconds, "" +participants);
+    }
+
     public void finishMeeting(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, PastMeetingInfoActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
         | Intent.FLAG_ACTIVITY_CLEAR_TOP
         | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        SaveToDatabase();
+
+        // Send -1 to signal that its the latest entry in the database
+        intent.putExtra("Database_ID", -1);
         startActivity(intent);
         finish();
     }
