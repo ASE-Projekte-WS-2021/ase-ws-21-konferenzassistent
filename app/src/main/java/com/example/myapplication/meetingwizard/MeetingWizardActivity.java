ce1922d484842926756f1fbace2d8be338a254a7
Max Huber <Maximilian2.Huber@stud.uni-regensburg.de>
2022-03-19 04:05:11 +0100

    fixed bug
    Bug gefixed wo sachen doppelt auftauchten

--- app/src/main/java/com/example/myapplication/meetingwizard/MeetingWizardActivity.java
+++ app/src/main/java/com/example/myapplication/meetingwizard/MeetingWizardActivity.java
@@ -173,8 +173,6 @@ public class MeetingWizardActivity extends AppCompatActivity implements OnAdapte
 
         advancedCountdownObjects.add(advancedCountdownObject);
         advancedCountdownObjects.add(advancedCountdownObject2);
-        advancedCountdownObjects.add(advancedCountdownObject);
-        advancedCountdownObjects.add(advancedCountdownObject2);
 
         // TODO: LOAD OBJECTS
 
